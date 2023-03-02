f8d7509237479eafbeed0e580662f260955f51c0
youknowfred <81190215+youknowfred@users.noreply.github.com>
2023-03-01 21:33:16 -0500

    keymap tweaks

--- keyboards/fingerpunch/personal/badwings/keymaps/youknowfred/keymap.c
+++ keyboards/fingerpunch/personal/badwings/keymaps/youknowfred/keymap.c
@@ -1,95 +1,114 @@
-/* Copyright 2021 Sadek Baroudi
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- * The `LAYOUT_badwings_base` macro is a template to allow the use of identical
- * modifiers for the default layouts (eg ALPHA_ALT, Colemak, Dvorak, etc), so
- * that there is no need to set them up for each layout, and modify all of
- * them if I want to change them.  This helps to keep consistency and ease
- * of use. K## is a placeholder to pass through the individual keycodes
- */
-
-#include "sadekbaroudi.h"
 #include QMK_KEYBOARD_H
 
-// clang-format off
-#define LAYOUT_badwings_base( \
-     K00, K10, K20, K30, K40,    K44, K34, K24, K14, K04,  \
-     K01, K11, K21, K31, K41,    K45, K35, K25, K15, K05,  \
-     K02, K12, K22, K32, K42,    K46, K36, K26, K16, K06,  \
-               K23, K33, K43,    K47, K37, K27  \
-  ) \
-  LAYOUT_wrapper( \
-        K00,            K10,            K20,            LT(_FUNCTION, K30),     K40,                      K44,                   LT(_FUNCTION, K34),    K24,              K14,            K04,         \
-        LCTL_T(K01),    LGUI_T(K11),    LALT_T(K21),    LSFT_T(K31),            K41,                      LT(_MOUSE, K45),       RSFT_T(K35),           RALT_T(K25),      RGUI_T(K15),    RCTL_T(K05), \
-        K02,            K12,            K22,            K32,                    K42,                      K46,                   K36,                   K26,              K16,            K06,         \
-                                        K23,            LT(_NAVIGATION,K33),    LT(_FUNCTION,K43),        LT(_MEDIA,K47),        LT(_SYMBOLS,K37),      K27 \
-    )
-
-/* Re-pass though to allow templates to be used */
-#define LAYOUT_badwings_base_wrapper(...)       LAYOUT_badwings_base(__VA_ARGS__)
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-    [_ALPHA_ALT] = LAYOUT_badwings_base_wrapper(
-        _________________ALPHA_ALT_L1_________________, _________________ALPHA_ALT_R1_________________,
-        _________________ALPHA_ALT_L2_________________, _________________ALPHA_ALT_R2_________________,
-        _________________ALPHA_ALT_L3_________________, _________________ALPHA_ALT_R3_________________,
-                                             __ALPHA_ALT_THUMBS_6__
-    ),
-
-    [_ALPHA] = LAYOUT_badwings_base_wrapper(
-        __________________ALPHA_L1____________________, __________________ALPHA_R1____________________,
-        __________________ALPHA_L2____________________, __________________ALPHA_R2____________________,
-        __________________ALPHA_L3____________________, __________________ALPHA_R3____________________,
-                                              __ALPHA_THUMBS_6__
-    ),
-
-    [_NAVIGATION] = LAYOUT_wrapper(
-        ________________NAVIGATION_1_______________, _________________NUMPAD_1__________________,
-        ________________NAVIGATION_2_______________, _________________NUMPAD_2__________________,
-        ________________NAVIGATION_3_______________, _________________NUMPAD_3__________________,
-                           _______, _______, KC_TAB, KC_BSPC, KC_SPACE, KC_DOT
-    ),
-
-    [_SYMBOLS] = LAYOUT_wrapper(
-        ________________SYMBOLS_L1_________________, ________________SYMBOLS_R1_________________,
-        ________________SYMBOLS_L2_________________, ________________SYMBOLS_R2_________________,
-        ________________SYMBOLS_L3_________________, ________________SYMBOLS_R3_________________,
-                            _______, KC_ENT, KC_DEL,  KC_BSPC, _______, _______
-    ),
-
-    [_FUNCTION] = LAYOUT_wrapper(
-        ________________SHIFTNAV_1_________________, ________________FUNCTION_1_________________,
-        ________________SHIFTNAV_2_________________, ________________FUNCTION_2_________________,
-        ________________SHIFTNAV_3_________________, ________________FUNCTION_3_________________,
-                          _______, _______, _______, N_DEL_LINE, KC_SPACE, _______
-    ),
-
-    [_MEDIA] = LAYOUT_wrapper(
-        ___________________RGB_1___________________, _________________MACROS_1__________________,
-        ___________________RGB_2___________________, _________________MACROS_2__________________,
-        ___________________RGB_3___________________, _________________MACROS_3__________________,
-                          _______, _______, _______, _______, _______, _______
-    ),
-
-    [_MOUSE] = LAYOUT_wrapper(
-        _______________AUTO_MOUSE_1________________, ___________________BLANK___________________,
-        _______________AUTO_MOUSE_2________________, ___________________BLANK___________________,
-        _______________AUTO_MOUSE_3________________, ___________________BLANK___________________,
-                    _______, KC_MS_BTN1, KC_MS_BTN3, KC_MS_BTN3, KC_MS_BTN2, _______
-    )
+enum layers {
+  _ALPHA,
+  _SYMBOL,
+  _NUMBER,
+  _NAVIGATION,
+  LAYER_LENGTH
 };
+
+
+enum tapdances {
+  TD_QESC,
+  TD_SBKT,
+  TD_CBKT,
+  TD_PARN,
+  TD_LTGT,
+  TD_ATAB,
+  TAPDANCE_LENGTH
+};
+
+enum combos {
+  COMBO_NAVIGATION,
+  COMBO_LENGTH
+};
+
+
+// begin tapdances
+#define KC_QESC     TD(TD_QESC)
+#define KC_SBKT     TD(TD_SBKT)
+#define KC_CBKT     TD(TD_CBKT)
+#define KC_PARN     TD(TD_PARN)
+#define KC_LTGT     TD(TD_LTGT)
+#define KC_ATAB     TD(TD_ATAB)
+
+#define KC_GUIX     LGUI_T(KC_X)
+#define KC_ALTC     LALT_T(KC_C)
+
+// oneshots
+#define KC_OSFT   OSM(MOD_LSFT)
+#define KC_OALT   OSM(MOD_LALT)
+
+// layer changing
+#define KC_OSYM   OSL(_SYMBOL)
+#define KC_ONUM   LT(_NUMBER, KC_BSPC)
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch(keycode) {
+        case KC_GUIX:
+        case KC_ALTC:
+            return TAPPING_TERM * 2;
+        default:
+            return TAPPING_TERM;
+    }
+}
+
+// tapdances
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_QESC]   = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESC),
+    [TD_SBKT]   = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_RBRC),
+    [TD_CBKT]   = ACTION_TAP_DANCE_DOUBLE(KC_LCBR, KC_RCBR),
+    [TD_PARN]   = ACTION_TAP_DANCE_DOUBLE(KC_LPRN, KC_RPRN),
+    [TD_LTGT]   = ACTION_TAP_DANCE_DOUBLE(KC_LABK, KC_RABK),
+    [TD_ATAB]   = ACTION_TAP_DANCE_DOUBLE(KC_A, KC_TAB)
+};
+// end tapdances
+
+uint16_t COMBO_LEN = COMBO_LENGTH;
+
+const uint16_t PROGMEM combo_navigation[] = { KC_OSYM, KC_ONUM, COMBO_END };
+combo_t key_combos[] = {
+  [COMBO_NAVIGATION]        = COMBO(combo_navigation, OSL(_NAVIGATION)),
+};
+
+uint16_t get_combo_term(uint16_t index, combo_t *combo) {
+  switch(index) {
+    case COMBO_NAVIGATION: // extending the combo term here helps reduce sticky layers some more.
+      return 250;
+    default:
+      return COMBO_TERM;
+  }
+}
+// end combos
+
+
+// begin layers
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_ALPHA] = LAYOUT(
+    KC_QESC,    KC_W,       KC_E,       KC_R,       KC_T,             KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       \
+    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,             KC_H,       KC_J,       KC_K,       KC_L,       KC_QUOT,    \
+    KC_Z,       KC_GUIX,    KC_ALTC,    KC_V,       KC_B,             KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    \
+                            KC_LCTL,    KC_OSYM,    KC_OSFT,          KC_SPC,     KC_ONUM,    KC_ENT
+  ),
+  [_SYMBOL] = LAYOUT(
+    KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,          KC_CIRC,     KC_AMPR,   KC_ASTR,    KC_PIPE,    KC_NO,      \
+    KC_GRV,     KC_TILD,      KC_UNDS,    KC_EQL,     KC_NO,            KC_SBKT,     KC_CBKT,   KC_PARN,    KC_LTGT,  KC_BSLS,  \
+    KC_NO,      KC_NO,      KC_PLUS,    KC_MINS,    KC_NO,            KC_NO,       KC_NO,     KC_COLN,    KC_DOT,     KC_SCLN,    \
+                             KC_LCTL,    KC_OSYM,    KC_OSFT,          KC_SPC,     KC_ONUM,    KC_ENT
+  ),
+  [_NUMBER] = LAYOUT(
+    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,            KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      \
+    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,             KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       \
+    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,            KC_NO,      KC_NO,      KC_NO,      KC_DOT,     KC_NO,      \
+                            KC_LCTL,    KC_OSYM,    KC_OSFT,          KC_SPC,     KC_ONUM,    KC_ENT
+  ),
+  [_NAVIGATION] = LAYOUT(
+    KC_NO,      KC_F2,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      KC_HOME,    KC_UP,      KC_END,     KC_BSPC,    \
+    KC_TAB,     KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_ENT,     \
+    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_DEL,     \
+                            KC_LCTL,    KC_OSYM,    KC_OSFT,        KC_SPC,     KC_ONUM,    KC_ENT
+  )
+};
+
+
--- keyboards/fingerpunch/personal/badwings/keymaps/youknowfred/rules.mk
+++ keyboards/fingerpunch/personal/badwings/keymaps/youknowfred/rules.mk
@@ -1,3 +1,7 @@
+# Bad Wings Specific
+TRACKPAD_ENABLE 		= yes
+
+# General QMK
 GRAVE_ESC_ENABLE		= no
 SPACE_CADET_ENABLE 	= no
 
@@ -5,4 +9,3 @@ CAPS_WORD_ENABLE		= yes
 KEY_OVERRIDE_ENABLE	= yes
 COMBO_ENABLE				= yes
 TAP_DANCE_ENABLE		= yes
-TRACKPAD_ENABLE        = yes
