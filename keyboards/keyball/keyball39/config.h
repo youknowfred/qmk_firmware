8d0cbc7979b737684520c431ddda5fca6329e78d
youknowfred <81190215+youknowfred@users.noreply.github.com>
2024-02-24 19:50:02 -0500

    Update config.h

--- keyboards/keyball/keyball39/config.h
+++ keyboards/keyball/keyball39/config.h
@@ -68,3 +68,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // To squeeze firmware size
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
+
+#ifdef CONVERT_TO=blok
+#else
+#endif
